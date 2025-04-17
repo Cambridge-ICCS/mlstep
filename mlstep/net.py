920bb8ebf4aa668a633051e5b23562fc3a74265b
Joe Wallwork <jw2423@cam.ac.uk>
2025-04-17 15:40:08 +0100

    Rework fcnn docstring; add max_nhsteps kwarg

--- mlstep/net.py
+++ mlstep/net.py
@@ -1,26 +1,43 @@
 """
 Module containing neural network architectures.
 """
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 __all__ = ["FCNN"]
 
+
 class FCNN(nn.Module):
     """
-    FCNN with a single hidden layer and ReLU activation function that accepts a vector
-    of real-valued input data and returns a single scalar natural number in the range
-    [1, 5].
+    Simple FCNN architecture designed to estimate the number of timestep length halvings
+    required by a solver.
+
+    The FCNN accepts a vector of real-valued input data and returns a single scalar
+    natural number for the number of halving steps required. It has a single hidden
+    layer and uses a ReLU activation function.
+
+    The input size is set upon constructing the class, as is the output size, which
+    determines the maximum permissible number of halving steps. The number of halving
+    steps is treated as a categorical variable, with each category corresponding to a
+    non-negative integer. Note that the output size is equal to the maximum number of
+    halving steps plus one, to account for the fact that in many cases the initial
+    choice of timestep is sufficient, i.e. zero halvings are required.
+
+    The number of neurons in the hidden layer is also configurable upon constructing the
+    class.
     """
-    def __init__(self, input_size, hidden_size=50):
+
+    def __init__(self, input_size, max_nhsteps=5, hidden_size=50):
         """
         :param input_size: Size of the input vector.
+        :param max_nhsteps: Maximum permissible number of halving steps (defaults to 5).
         :param hidden_size: Size of the hidden layer (defaults to 50).
         """
-        super(FCNN, self).__init__()
+        super().__init__()
         self.hidden = nn.Linear(input_size, hidden_size)
-        self.output = nn.Linear(hidden_size, 5)  # 5 outputs for classes [1, 2, 3, 4, 5]
+        self.output = nn.Linear(hidden_size, max_nhsteps + 1)
 
     def forward(self, x):
         """
@@ -28,4 +45,4 @@ class FCNN(nn.Module):
         """
         x = F.relu(self.hidden(x))
         x = self.output(x)
-        return torch.argmax(F.softmax(x, dim=1), dim=1) + 1  # Shift to range [1, 5]
+        return torch.argmax(F.softmax(x, dim=1), dim=1)
