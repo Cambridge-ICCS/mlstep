2c37af412029723e6be21bae71b8fe2b84452e32
Joe Wallwork <22053413+jwallwork23@users.noreply.github.com>
2025-07-16 11:55:58 +0100

    Add utilities for loading target and feature data from NetCDF files (#4)
    * Data utils module
    * Unit tests for data_utils
    * netCDF4 and numpy now dependencies
    * Add test CDL files
    * Infrastructure for generating NetCDF files and testing
    * Add CI step for building NetCDF files

---
+++ mlstep/data_utils.py
@@ -0,0 +1,138 @@
+"""Utilities for handling NetCDF data files."""
+
+import os
+
+import netCDF4
+import torch
+
+__all__ = ["NetCDFDataLoader"]
+
+
+class NetCDFDataLoader:
+    """Class for handling loading data from NetCDF files."""
+
+    def __init__(self, features_1d, features_2d, num_timesteps, data_dir="data"):
+        """
+        Initialise the NetCDFDataLoader.
+
+        :param features_1d: List of 1D feature variable names to load.
+        :param features_2d: List of 2D feature variable names to load.
+        :param num_timesteps: Number of NetCDF files to load.
+        :param data_dir: Directory where the NetCDF files are stored (defaults to
+            "data").
+        """
+        self._features_1d = features_1d
+        self._features_2d = features_2d
+        self._features = None
+        self.num_timesteps = num_timesteps
+        if not os.path.exists(data_dir):
+            errmsg = f"Data directory '{data_dir}' does not exist."
+            raise IOError(errmsg)
+        self.data_dir = data_dir
+        self._max_nhsteps = None
+
+    @property
+    def max_nhsteps(self):
+        """:returns: The maximum number of halving steps."""
+        if self._max_nhsteps is None:
+            errmsg = (
+                "Max halving steps have not been set. Call load_target_data() first."
+            )
+            raise RuntimeError(errmsg)
+        return self._max_nhsteps
+
+    @property
+    def features(self):
+        """:returns: List of feature variable names."""
+        if self._features is None:
+            errmsg = "Features have not been loaded. Call load_feature_data() first."
+            raise RuntimeError(errmsg)
+        return self._features
+
+    def _read_nc(self, variable, timestep, dim):
+        """
+        Read a feature from a feature data NetCDF file.
+
+        :param dim: Number of dimensions in the feature data.
+        :returns: Torch tensor containing the feature data
+        """
+        filename = os.path.join(self.data_dir, f"{variable}_{timestep}.nc")
+        with netCDF4.Dataset(filename, "r") as nc:
+            return torch.Tensor(
+                nc.variables[variable][:] if dim == 1 else nc.variables[variable][:][:]
+            )
+
+    def load_target_data(self):
+        """
+        Load halving steps data from NetCDF files.
+
+        The halving steps data are converted to a binary matrix format, where entry
+        :math:`(i,j)` is one if entry i of nhsteps takes the value :math:`2^j` and zero
+        otherwise.
+
+        This method has the side-effect of determining the `max_nhsteps` attribute for
+        the maximum number of halving steps across all target data.
+
+        :returns: The number of halving steps for each grid-box and timestep as a binary
+            matrix (rank-2 tensor).
+        """
+        # Read the eventual number of timesteps from the NetCDF files and convert them
+        # to numbers of halving steps
+        nhsteps = torch.hstack(
+            [
+                torch.log2(self._read_nc("ncsteps", timestep, 1)).round().int()
+                for timestep in range(1, self.num_timesteps + 1)
+            ]
+        )
+
+        # Stash the maximum number of halving steps
+        self._max_nhsteps = int(nhsteps.max().item())
+
+        # Prepare the halving steps data for classification
+        target_data = torch.zeros((len(nhsteps), self.max_nhsteps + 1), dtype=torch.int)
+        for i, nhstep in enumerate(nhsteps):
+            target_data[i, nhstep] = 1
+        return target_data
+
+    def _load_feature_data(self, dim, dtype=torch.float):
+        """
+        Load feature data from NetCDF files for a given dimension.
+
+        :param dim: Number of dimensions in the feature data.
+        :param dtype: Data type to use.
+        :returns: List of tensors containing feature data.
+        """
+        assert dim in (1, 2), "Only feature data with 1 or 2 dimensions are supported."
+        return [
+            torch.hstack(
+                [
+                    self._read_nc(feature, timestep, dim).to(dtype=dtype)
+                    for timestep in range(1, self.num_timesteps + 1)
+                ]
+            )
+            for feature in (self._features_1d if dim == 1 else self._features_2d)
+        ]
+
+    def load_feature_data(self, dtype=torch.float):
+        """
+        Load feature data from NetCDF files.
+
+        This method has the side-effect of determining the overall `features` attribute,
+        which lists each individual component of the feature data (i.e., 1D feature
+        arrays and each 1D slice of 2D feature arrays.
+
+        :param dtype: Data type to use.
+        :returns: Feature data as a rank-2 tensor.
+        """
+        # Load 1D feature data
+        feature_data = self._load_feature_data(1, dtype=dtype)
+        self._features = self._features_1d
+
+        # Load 2D feature data and update features list
+        feature_data_2d = self._load_feature_data(2, dtype=dtype)
+        for feature_name, features in zip(self._features_2d, feature_data_2d):
+            self._features.extend(f"{feature_name}_{j}" for j in range(len(features)))
+            feature_data += features
+
+        # Stack all feature data into a rank-2 tensor
+        return torch.stack(feature_data, dim=1)
