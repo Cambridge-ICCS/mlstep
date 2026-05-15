d8c08db719ec9e65038ee92c0a10c94e228ebd48
Joe Wallwork <jw2423@cam.ac.uk>
2026-05-15 16:16:32 +0100

    Temporary testing code - TODO: remove or turn into an example/test

--- examples/ukca/write_out.py
+++ examples/ukca/write_out.py
@@ -13,3 +13,15 @@ model = FCNN(*input_sizes, max_nhsteps=5, hidden_size=50)
 # torch.save(model, "mlstep_model_pytorch.pt")
 scripted_model = torch.jit.script(model)
 scripted_model.save("mlstep_model_torchscript.pt")
+
+# TODO: Remove this test code
+batch_size = 10
+loss = torch.nn.CrossEntropyLoss()
+inputs = [
+    torch.randn(batch_size, input_size)
+    for input_size in input_sizes
+]
+output = model(*inputs)
+print(f"output:\n{output}")
+target = torch.zeros(batch_size, dtype=torch.long)
+print(f"loss:\n{loss(output, target)}")
